f995722d9f290bf3302a997a9ddb888626696304
paulBeaudet <inof8or@gmail.com>
2014-10-10 19:53:34 -0400

    alligned buttons and pagers

--- hardware.ino
+++ hardware.ino
@@ -4,16 +4,18 @@
 
 Adafruit_PWM pagers = Adafruit_PWM();
 
+int PWMintensity = 1000; // Adjusts the intensity of the pwm
+
 //----------PINOUT DEFINITIONS-------------------
-byte buttons[] = { 5,4,6,9,7,8,10,3,2,11, };
-// !! ---set the desired button pins here--- !!
-#define NUMBUTTONS sizeof(buttons)
+byte buttons[] = { 10,9,7,8,5,4,6,3,2,11 };// pin out oppisite to pagers
+// pins can be aligned in software: try to do it in hardware
+#define NUMBUTTONS sizeof(buttons) // up to 16 possible
 #define NUMPAGERS 8 // can use up to 16
 //------------HARDWARE SETUP --------------------------
 void pagersUp() // to speed up i2c, go into 'fast 400khz I2C' mode
 {               // might not work when sharing the I2C bus
   pagers.begin();
-  pagers.setPWMFreq(1600);  // This is the maximum PWM frequency
+  pagers.setPWMFreq(PWMintensity);  // This is the maximum PWM frequency
   uint8_t twbrbackup = TWBR;// save I2C bitrate
   // must be changed after calling Wire.begin() (inside pwm.begin())
   TWBR = 12; // upgrade to 400KHz!   
@@ -25,12 +27,12 @@ void buttonUp()// it's cold out there, set up the buttons
 }//pull-up -> 20k 5v rail| Pin-> button -> ground:reads low pressed
 
 //-------------- actuating pagers---------------
-void patternVibrate(int pins, int pwm)
+void patternVibrate(int pins)
 { //set state of all pagers in one function
   for (byte i=0; i<NUMPAGERS; i++) 
   { // imagine incoming int as an array of 16 bits, one for each pager
-    if (pins & (1 << i)) { pagers.setPWM( i, pwm, 0); }
-    else/*set pager off*/{ pagers.setPWM( i, 0, pwm); }
+    if (pins & (1 << i)) { pagers.setPWM( i, PWMintensity, 0); }
+    else/*set pager off*/{ pagers.setPWM( i, 0, 0); }
   }
 }
 //---------------Sampling buttons-------------
@@ -44,3 +46,22 @@ int buttonSample()
   }//otherwise                          set the bit low
   return sample;
 }// returning and int, allows 16 possible buttons states to be combined
+
+void pagerTesting()
+{
+  //testing capibilities of the adafruit shield soon
+  for (int i=0; i<4096; i += 8) 
+  {
+    for (byte pwmnum=0; pwmnum < 8; pwmnum++) 
+    {
+      pagers.setPWM(pwmnum, 10, (i + (4096/16)*pwmnum) % 4096 );
+    }
+  }
+  for (int i=0; i<4096; i += 8) 
+  {
+    for (byte pwmnum=0; pwmnum < 8; pwmnum++) 
+    {
+      pagers.setPWM(pwmnum, 0, 0);
+    }
+  }
+}
