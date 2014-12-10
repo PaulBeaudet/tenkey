6ed122c23619971f76d440bbc131012407d645c4
paulBeaudet <inof8or@gmail.com>
2014-12-10 16:10:26 -0500

    works a bit better still getting no returned data error

--- neoHost-LinuxKeyhandler/keyPrinter.py
+++ neoHost-LinuxKeyhandler/keyPrinter.py
@@ -1,32 +1,33 @@
 #! /usr/bin/python
-
-#    Serial Reader for ARDUINO
-#    usefull when tail -f /dev/ttyXXXX doesn't work
-#    Change ttyACM0 for your own tty interface
+# Copyright 2014 Paul Beaudet -- GPLv2, See license for reuse detials
+# xdotool needs to be installed for this script to work
+# sudo apt-get install xdotool <--
+# Serial host testing for the Neotype keyer
 import serial, time, subprocess
 
 # The second argument is the baudrate,
 # change according to the baudrate you gave to your Serial.begin command
-port0 = serial.Serial("/dev/ttyACM0", 9600)
-
-# To avoid communication failure due to bad timings
-port0.setDTR(True)
+port0 = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 time.sleep(1)
-port0.setDTR(False)
 
 #output function
 def keyOut(letter):
-    if letter >= ' ':
-        ps = subprocess.call(['xdotool','key',letter])
+    if letter == chr(8): #BACKSPACE
+        subprocess.call(['xdotool','key','BackSpace'])#not working
+    if letter == ' ': #SPACEBAR
+        subprocess.call(['xdotool','key','space'])
+    if letter > ' ':
+        subprocess.call(['xdotool','key',letter])
 
 #Main loop
 while True:
-    if port0.inWaiting() > 0:
-        try:
-            keyOut(port0.read())
-        except Exception, e:
-            print "marbles lost! ", e
-            continue
+    try:
+        incoming = port0.read()
+        if incoming > 0:    
+            keyOut(incoming)
+    except Exception, e:
+        print "error! ", e
+        continue
 
-#still getting an error, the python is also taking up a lot of process cycles doing all of this. 
-#device reports readiness to read but returned no data (device disconnected or multiple access on port?)
+#this works but I'm still getting the following error, the python is also taking up a lot of process cycles doing all of this.
+#device reports readiness to read but returned no data (device disconnected or multiple access on port? 
