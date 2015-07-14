2c128a01a5d96c5ce8ccd50cc1e73a32d07e855a
paulBeaudet <inof8or@gmail.com>
2015-07-14 15:27:22 -0400

    fixed comment spacing and added some information

--- tenkey_Neotype.ino
+++ tenkey_Neotype.ino
@@ -1,61 +1,64 @@
 /********tenkey_Neotype.ino*********
-* Copyright Paul Beaudet Created Fall 2014 <inof8or@gmail.com>
+* NOTE This is the main .ino sketch project
+  |--> parent folder name should be "tenkey_Neotype" to recognize as such
+* Compiled with Arduino.cc 1.6 IDE
+* Copyright Paul Beaudet originally created fall 2014 <inof8or@gmail.com>
 * Keyer that recieves and transmits text information from its user
-* This program is free open source software: See licenses.txt to understand 
+* This program is free open source software: See licenses.txt to understand
   reuse rights of any file associated with this project
 ********* See readme.md for hardware discription **************/
-#include <Wire.h>           //i2c
-#include <EEPROM.h>         //store mouse calibration
-#include "keyDefinitions.h" //define key numbers for various output types
-#include "pin_definitions.h"//Pin arrangements per type of board
-//switch board named file with your board found in "hardwareOptions" folder
-//eg -> yun.ino <-with-> hardwareOptions/uno.ino
+#include <Wire.h>           // i2c
+#include <EEPROM.h>         // store mouse calibration
+#include "keyDefinitions.h" // define key numbers for various output types
+#include "pin_definitions.h"// Pin arrangements per type of board
+// board named file with your board found in "hardwareOptions" folder
+// eg -> yun.ino <-with-> hardwareOptions/uno.ino
 
-#define MONITOR_BUTTONS 33 //signal to monitor buttons
-#define MONITOR_MODE   0   //goto default behavior for multi-mode functions
-#define TRIGGER        1   //set enter key to press : enterBehavior()
-#define DEFAULT_MODE   1   //potentiometer check
-#define NUMBERS_MODE   2   //outputFilter: Numbers
-#define MONITOR_PAGERS 2   //check pagerActivity();
-#define RECORD_CAT     2   //cat or record or record cat
-#define ADJUST_PWM     2   //potentiometer()
-#define ADJUST_TIMING  3   //potentiometer()
-#define LINE_SIZE      80  //cause why should a line be longer?
+#define MONITOR_BUTTONS 33 // signal to monitor buttons
+#define MONITOR_MODE   0   // goto default behavior for multi-mode functions
+#define TRIGGER        1   // set enter key to press : enterBehavior()
+#define DEFAULT_MODE   1   // potentiometer check
+#define NUMBERS_MODE   2   // outputFilter: Numbers
+#define MONITOR_PAGERS 2   // pagerActivity();
+#define RECORD_CAT     2   // cat or record or record cat
+#define ADJUST_PWM     2   // potentiometer()
+#define ADJUST_TIMING  3   // potentiometer()
+#define LINE_SIZE      80  // cause why should a line be longer?
 
-void setup()//setup the needed hardware  
+void setup()//setup the needed hardware
 {
-  pagersUp();          //pagers.ino: brings vibrating motor interface up
-  buttonUp();          //buttons.ino: brings button polling intreface up
-  serialInterfaceUp(); //yun/uno/leo.ino: brings serial output interface/s up
-  EEPROMsetup();       //yun/uno/leo.ino: manages first calibration session
+  pagersUp();          // pagers.ino: brings vibrating motor interface up
+  buttonUp();          // buttons.ino: brings button polling intreface up
+  serialInterfaceUp(); // yun/uno/leo.ino: brings serial output interface/s up
+  EEPROMsetup();       // yun/uno/leo.ino: manages first calibration session
 }
 
-void loop() 
+void loop()
 {
-  mainIOroutine();            //handles key input and output
-  feedbackAndRelease();       //controls pager feedback and release control
-  messageHandlr(MONITOR_MODE);//handles incoming messages
-  listenForMessage();         //grab potential messages over usb serial
-  potentiometer(MONITOR_MODE);//monitor potentiometer: pagers.ino 
-  mouseMovement();            //monitor joystick for mouse actions
-  serialBowl(MONITOR_MODE);   //check: terminal responses
+  mainIOroutine();            // handles key input and output
+  feedbackAndRelease();       // controls pager feedback and release control
+  messageHandlr(MONITOR_MODE);// handles incoming messages
+  listenForMessage();         // grab potential messages over usb serial
+  potentiometer(MONITOR_MODE);// monitor potentiometer: pagers.ino
+  mouseMovement();            // monitor joystick for mouse actions
+  serialBowl(MONITOR_MODE);   // check: terminal responses
 }
 
 //---- main chord interpertation flow control ----
 byte mainIOroutine()
 {
-  byte pressState = chordLoop(); //captures the current state of the buttons
+  byte pressState = chordLoop(); // captures the current state of the buttons
   if(pressState)
-  {   
-    if (pressState < 128)      //narrows values to letters
+  {
+    if (pressState < 128)      // narrows values to letters
     {
-      recordHandlr(pressState);//records presses to messageHandlr given active
-      keyOut(pressState);      //actuate the press as a keystroke
-      messageHandlr(TRIGGER);  //letters interupt messages
-    } 
+      recordHandlr(pressState);// records presses to messageHandlr given active
+      keyOut(pressState);      // actuate the press as a keystroke
+      messageHandlr(TRIGGER);  // letters interupt messages
+    }
     else if(pressState > 159){mouseClick(pressState);}//special cases
-    else{macros(pressState);} //in all other cases check for macros
-  }//macros are exempt from interupting messageHandlr
+    else{macros(pressState);} // in all other cases check for macros
+  } // macros are exempt from interupting messageHandlr
   return pressState;
 }
 
@@ -63,16 +66,16 @@ byte chordLoop()
 {
   static byte doubleActuation = 0;
 
-  byte actuation = 0;            //establish var that will be returned
-  if(doubleActuation)            //backspace needed to be sent last iteration
-  {                              //double actuation holds the intended char
+  byte actuation = 0;            // establish var that will be returned
+  if(doubleActuation)            // backspace needed to be sent last iteration
+  {                              // double actuation holds the intended char
     actuation = doubleActuation;
     doubleActuation = 0;
     return actuation;
   }
-  
-  buttonUpdate();                      //updates internal button state
-  int chord = trueChord(MONITOR_MODE); //monitor chord posibilities
+
+  buttonUpdate();                      // updates internal button state
+  int chord = trueChord(MONITOR_MODE); // monitor chord posibilities
   byte pressState = patternToChar(chord);
   byte hold = 0;
   if(doubleActuation = doubleToASCII(doubleEvent(pressState)))
@@ -86,8 +89,8 @@ byte chordLoop()
     hold = pressState;
   }
   byte varifiedHold = 0;
-  hold = holdHandlr(hold);                  //check if there was a hold
-  if(hold){varifiedHold = heldASCII(hold);} //only feed in hold events
+  hold = holdHandlr(hold);                  // check if there was a hold
+  if(hold){varifiedHold = heldASCII(hold);} // only feed in hold events
   else if(!buttonState(MONITOR_BUTTONS)){heldASCII(0);}//feed in release cases
 
   if(varifiedHold){actuation = varifiedHold;}
@@ -97,8 +100,8 @@ byte chordLoop()
 //---- feedback & state handling ----
 void feedbackAndRelease()
 {
-  static boolean held = false; 
-  //general press hold, for holdstates see conversions.ino -> heldASCII()
+  static boolean held = false;
+  // general press hold, for holdstates see conversions.ino -> heldASCII()
   int currentState = buttonState(MONITOR_BUTTONS);
   if( held && currentState == 0 ) //at the moment of a release
   {
@@ -108,7 +111,7 @@ void feedbackAndRelease()
     held = false;
   }
   else if(patternToChar(currentState))
-  { // if that state is a pattern 
+  { // if that state is a pattern
     if(pagerActivity(MONITOR_PAGERS) == false){patternVibrate(currentState);}
     held = true;
   }
@@ -116,8 +119,8 @@ void feedbackAndRelease()
 
 boolean vibInactive() //check other function controling vibrators
 { //extended AND opperation for readability
-  if(serialBowl(MONITOR_MODE)){return false;} //if terminal mode is printing
-  if(messageHandlr(MONITOR_MODE)){return false;} //if message mode is printing
+  if(serialBowl(MONITOR_MODE)){return false;} // if terminal mode is printing
+  if(messageHandlr(MONITOR_MODE)){return false;} // if message mode is printing
   if(pagerActivity(MONITOR_PAGERS)){return false;}
   return true; // as long as everything is inactive return true
 }
@@ -125,19 +128,19 @@ boolean vibInactive() //check other function controling vibrators
 //---- Special macro functions ----
 void macros(byte letter)
 {
-  if     (letter == 'a' + SPACEBAR){convertionMode(TRIGGER);}//toggle numbers
+  if     (letter == 'a' + SPACEBAR){convertionMode(TRIGGER);} // toggle numbers
   else if(letter == 'b' + SPACEBAR) // play message buffer
   {
     if(recordHandlr(MONITOR_MODE)){;}
     else{messageHandlr(RECORD_CAT);}
   }
   else if(letter == 'h' + SPACEBAR){alphaHint();} // play alphabetical hint
-  else if(letter == 'i' + SPACEBAR){potentiometer(ADJUST_PWM);}//Toggle to pwm
+  else if(letter == 'i' + SPACEBAR){potentiometer(ADJUST_PWM);} // Toggle to pwm
   else if(letter == 'j' + SPACEBAR){comboPress(LEFT_ALT,0,0);}
-  else if(letter == 'k' + SPACEBAR){keyOut(letter);}//toggle keyboard mode
+  else if(letter == 'k' + SPACEBAR){keyOut(letter);} // toggle keyboard mode
   else if(letter == 'l' + SPACEBAR){comboPress(LEFT_CTRL|LEFT_ALT,0,0);}
-  else if(letter == 'p' + SPACEBAR){potentiometer(DEFAULT_MODE);}//show value
-  else if(letter == 'r' + SPACEBAR){recordHandlr(TRIGGER);}//start recording
-  else if(letter == 's' + SPACEBAR){potentiometer(ADJUST_TIMING);}//toggle 
-  else if(letter == 't' + SPACEBAR){serialBowl(TRIGGER);}//toggle terminal
+  else if(letter == 'p' + SPACEBAR){potentiometer(DEFAULT_MODE);} // show value
+  else if(letter == 'r' + SPACEBAR){recordHandlr(TRIGGER);} // start recording
+  else if(letter == 's' + SPACEBAR){potentiometer(ADJUST_TIMING);} // toggle
+  else if(letter == 't' + SPACEBAR){serialBowl(TRIGGER);} // toggle terminal
 }
