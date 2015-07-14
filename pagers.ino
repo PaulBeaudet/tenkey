f1bcfe7011d3d46dd2ae144d23443aa55c70c79b
paulBeaudet <inof8or@gmail.com>
2015-07-14 12:28:14 -0400

    audited for readability cleaned up pot function

--- pagers.ino
+++ pagers.ino
@@ -1,17 +1,17 @@
-//hardware.ino  Copyright Paul Beaudet 2014 See license for reuse info
-//depends on wire and Adafruit_PWM
-//Uses pins A4 and A5 or expantion pins on leo
+// hardware.ino  Copyright Paul Beaudet 2014 See license for reuse info
+// depends on wire and Adafruit_PWM
+// pins A4 and A5 or expantion pins on leo
 //---------------- Haptics / Pagers / Vib motors -----------------
-#include "Adafruit_PWM.h" // see readme for harware notes
-#define NUMPAGERS 8 // can use up to 16
+#include "Adafruit_PWM.h"             // see readme for harware notes
+#define NUMPAGERS 8                   // can use up to 16
 #define COUNTBACKPAGERS 8 - 1
-Adafruit_PWM pagers = Adafruit_PWM();// create pagers object
+Adafruit_PWM pagers = Adafruit_PWM(); // create pagers object
 
 void pagersUp() // to speed up i2c, go into 'fast 400khz I2C' mode
 {               // might not work when sharing the I2C bus
   pagers.begin();
-  pagers.setPWMFreq(1600);  // 1600 is the maximum PWM frequency
-  uint8_t twbrbackup = TWBR;// save I2C bitrate
+  pagers.setPWMFreq(1600);   // 1600 is the maximum PWM frequency
+  uint8_t twbrbackup = TWBR; // save I2C bitrate
   // must be changed after calling Wire.begin() (inside pwm.begin())
   TWBR = 12; // upgrade to 400KHz!
 }
@@ -24,13 +24,13 @@ int pagerIntensity(int intensityChange)
 }
 
 void patternVibrate(int pins)
-{ //set state of all pagers in one function
+{ // set state of all pagers in one function
   byte j = COUNTBACKPAGERS; // For oppisite direction, if forward remove
-  for (byte i=0; i<NUMPAGERS; i++) 
+  for (byte i=0; i<NUMPAGERS; i++)
   { // incoming int set bit by bit: high bits: pagers need to be active
     if (pins & (1 << i)) { pagers.setPWM( j, 0, pagerIntensity(MONITOR_MODE));}
     else                 { pagers.setPWM( j, 0, 0); }//change j to i if forward
-    j--;// remove if forward oriented
+    j--; // remove if forward oriented
   }
 }
 
@@ -42,84 +42,84 @@ boolean ptimeCheck(uint32_t durration)
     ptimer[1]=durration;          // set durration
     ptimer[0]=millis();           // note the time set
   }                               // if the durration has elapsed return true
-  else if(millis() - ptimer[0] > ptimer[1]){return true;}//time has passed  
-  return false;                   //time has yet to pass
+  else if(millis() - ptimer[0] > ptimer[1]){return true;}//time has passed
+  return false;                   // time has yet to pass
 }
 
 boolean pagerActivity(byte activityMode)
 {
   static boolean activityReading = false;
-  
+
   if(activityMode == 2){return activityReading;}
   else{activityReading = activityMode;}
 }
 
-boolean hapticMessage(byte letter, int spacing = 0) 
+boolean hapticMessage(byte letter, int spacing = 0)
 { // updating function; passing a string sets course of action
   static boolean animated = false; // animated or typical letter?
-  static int timing = 150; //default timing
-  
-  if(spacing){timing = spacing; return false;}//change timing call
-  
-  if(letter)
-  {    
+  static int timing = 150;         // default durration of letter play
+
+  if(spacing){timing = spacing; return false;} // change timing call
+
+  if(letter)         // given a value comes through: init case
+  {
     if(byte validPatern = charToPattern(letter))
-    {
-      ptimeCheck(timing);
-      patternVibrate(validPatern);
-      animated = false;
+    {                              // if the letter converts to a pattern
+      ptimeCheck(timing);          // start the timer for specified durration
+      patternVibrate(validPatern); // signal the pagers to vibrate
+      animated = false;            // this is a non-animated pattern
     }
     else if(byte validAnimation = getFrame(0, letter))
-    {//if 0 frame is availible for this letter
-      int adjustedTime = timing / 8 + timing;
-      ptimeCheck(adjustedTime/NUMPAGERS); // a fraction of alotted time
-      patternVibrate(validAnimation);
-      animated = true;
-    } // in this way invalid entries are skipped message reads true for such
-    return false;//why bother checking time... we just set it
+    {     // if first frame is availible for this value then it is an animation
+      int adjustedTime = timing / 8 + timing; // calculate total normal timing
+      ptimeCheck(adjustedTime/NUMPAGERS);  // set frame durration (total/frames)
+      patternVibrate(validAnimation);      // vibrate first frame
+      animated = true;                     // This is an animation
+    }                                      // invalid entries are skipped
+    return false; // why bother checking time... we just set it
   }
-  //---------- 0 or "monitor" case ------- aka no letter check if done
-  if (animated){return animatedProcess(timing);}
-  else {return typicalLetter(timing);}
+  //--- 0 or "monitor" case --- given no value passed as letter, check timing
+  if (animated){return animatedProcess(timing);} // run animation logic
+  else {return typicalLetter(timing);}           // run letter display logic
 }
 
 boolean typicalLetter(int timing)
 {
   static boolean touchPause= 0; // pause between displays
-  
-  if(ptimeCheck(0))
-  {               //time to "display" a touch / pause elapsed
-    if(touchPause)//given that we are at the pause stage FINAL
-    {             //this case allows for a pause after "display"
-      touchPause=!touchPause; //prep for next letter
-      return true;//Send confirmation this letter has been "played"
+
+  if(ptimeCheck(0)) // check if time is up
+  {                 // time to "display" a touch / pause elapsed
+    if(touchPause)  // given that we are at second pause stage
+    {               // this case allows for a pause after "display"
+      touchPause=!touchPause; // reset stage
+      return true;  // Send confirmation this letter has been "played"
     }
-    else          //durring the letter buzz phase
+    else                      // durring the letter buzz phase
     {
-      touchPause=!touchPause;    //flag pause time to start
-      patternVibrate(0);         //stop letter feedback
-      ptimeCheck(timing/8);//set pause time
+      touchPause=!touchPause; // flag pause time to start
+      patternVibrate(0);      // stop letter feedback
+      ptimeCheck(timing/8);   // set pause time (1/8 of active durration)
     };
   }
-  return false;  //signals letter in process of being played
+  return false; // signals letter still in process of being played
 }
 
 boolean animatedProcess(int timing)
 {
   static byte frame = 0;
- 
-  if(ptimeCheck(0))//if timer has been tripped
+
+  if(ptimeCheck(0))        // if timer has been tripped
   {
-    frame++;          //zero frame is accounted for in hapticMessage
+    frame++;               // zero frame is accounted for in hapticMessage
     if(frame == NUMPAGERS) // reached maxium number of frames
     {
-      frame = 0;     //Start back at frame zero
-      getFrame(0,TRIGGER); //reset framer
-      return true;   // animation complete
+      frame = 0;           // Start back at frame zero
+      getFrame(0,TRIGGER); // reset framer
+      return true;         // animation complete
     }
-    patternVibrate(getFrame(frame)); //start to play frame
-    int adjustedTime = timing / 8 + timing;
-    ptimeCheck(adjustedTime/NUMPAGERS);    //devides frame increments
+    patternVibrate(getFrame(frame));       // start to play frame
+    int adjustedTime = timing / 8 + timing;// calculate total time
+    ptimeCheck(adjustedTime/NUMPAGERS);    // devide time by frames to be played
   }
   return false;
 }
@@ -130,36 +130,36 @@ boolean messageHandlr(byte mode)
   static char lineBuffer[LINE_SIZE]={};
   static byte pos = 0; // in this way buffer can be no greater than 255
   static boolean playFlag = 0;
-  
+
   if(mode == MONITOR_MODE )
-  { // returns play state and handles message output 
-    if(playFlag) 
-    { 
-      if(hapticMessage(MONITOR_MODE))//reads true when current letter done
-      {                              //also reads true on start from setup
+  { // returns play state and handles message output
+    if(playFlag)
+    {
+      if(hapticMessage(MONITOR_MODE))   // reads true when current letter done
+      {                                 // also reads true on start from setup
         if(lineBuffer[pos] == NEW_LINE)
         {// Check for end case before updating further
-          removeThisMany(pos);//backspace printed chars
-          pos = 0; playFlag = false;//reset possition and playflag
-          return false; // play has finished
+          removeThisMany(pos);          // backspace printed chars
+          pos = 0; playFlag = false;    // reset possition and playflag
+          return false;                 // play has finished
         }// END CASE: MESSAGE HAS BEEN PRINTED AND REMOVED
-        hapticMessage(lineBuffer[pos]);       //start next letter vib
-        keyOut(lineBuffer[pos]);              //tx next letter
-        pos++;                                //increment read possition
-      }//false == waiting -> return -> continue main loop
+        hapticMessage(lineBuffer[pos]); // start next letter vib
+        keyOut(lineBuffer[pos]);        // tx next letter
+        pos++;                          // increment read possition
+      } // false == waiting -> return -> continue main loop
     }
   }
   else if(mode == TRIGGER) // 1 Trigger interupt
   { // triggering mechinism for message interuption
     if(playFlag)
     {
-      removeThisMany(pos);    //backspace printed chars
-      pos = 0; playFlag = false;  //reset possition and playflag
+      removeThisMany(pos);       // backspace printed chars
+      pos = 0; playFlag = false; // reset possition and playflag
     }
   }
-  else if(mode == RECORD_CAT){playFlag = true;}//signal play to start
+  else if(mode == RECORD_CAT){playFlag = true;} // signal play to start
   //------------------ letters cases -----------------
-  else if(mode == BACKSPACE){pos--;}//delete buffer entry-> happens in record
+  else if(mode == BACKSPACE){pos--;} // delete buffer entry-> happens in record
   else if(mode == NEW_LINE)
   {
     lineBuffer[pos] = NEW_LINE;    // This signifies end of message!
@@ -174,31 +174,31 @@ boolean messageHandlr(byte mode)
   return playFlag;
 }
 
-//******* Serial receive message *************** 
+//******* Serial receive message ***************
 void listenForMessage()
-{ 
+{
   while(Serial.available())
   {
     char singleLetter = (char)Serial.read();
-    messageHandlr(singleLetter);//fills up the handlr's lineBuffer
+    messageHandlr(singleLetter); // fills up the handlr's lineBuffer
     if(singleLetter == NEW_LINE)
     {
-      messageHandlr(TRIGGER);//In the middle of something? don't care
-      messageHandlr(RECORD_CAT);// flag to play-> ie concat recording 
+      messageHandlr(TRIGGER);    // In the middle of something? don't care
+      messageHandlr(RECORD_CAT); // flag to play-> ie concat recording
       return;
     }
   }
 }
-//********** message recording *********************** 
+//********** message recording ***********************
 boolean recordHandlr(byte mode)
 {
-  static boolean active = 0; // var for the outside world to undersand state
-  static byte recordLength = 0; // Keep track 
-  
+  static boolean active = 0;    // var for the outside world to undersand state
+  static byte recordLength = 0; // Keep track
+
   if(mode == MONITOR_MODE){return active;}
   if(mode == TRIGGER)
-  {// trigger/toggle recording step
-    active = !active; // toggle recording state
+  {                               // trigger/toggle recording step
+    active = !active;             // toggle recording state
     if(active){fastToast("rec");} // flash recording warning
     else // in the case recording has been toggled inactive
     {
@@ -207,12 +207,12 @@ boolean recordHandlr(byte mode)
     }
   }
   else if(mode == CARIAGE_RETURN)
-  { // finish recording step
+  {                                              // finish recording step
     messageHandlr(NEW_LINE);
-    active = false; // end activity
+    active = false;                              // end activity
     removeThisMany(recordLength);recordLength=0; // remove recording
   }
-  else if(active && mode < 128) // valid input situations
+  else if(active && mode < 128)                  // valid input situations
   { // record step: passes incoming letter to the messageHandlr
     messageHandlr(mode);
     if(mode == BACKSPACE){recordLength--;}
@@ -222,7 +222,7 @@ boolean recordHandlr(byte mode)
 }
 //****************Output Functions ****************************
 
-void fastToast(char message[])//quick indication message
+void fastToast(char message[]) // quick indication message
 { // TODO include haptics
   for(byte i=0;message[i];i++){keyOut(message[i]);delay(5);}
   delay(5);
@@ -234,47 +234,47 @@ void fastToast(char message[])//quick indication message
 void alphaHint()
 {
   for(byte i=97;i<123;i++){messageHandlr(i);} // for all the letters
-  messageHandlr(NEW_LINE);//need to know where the end of the statement is
-  messageHandlr(RECORD_CAT);//mee-oow! tell it to play the message
+  messageHandlr(NEW_LINE);   // need to know where the end of the statement is
+  messageHandlr(RECORD_CAT); // mee-oow! tell it to play the message
 }
 
 void removeThisMany(int numberOfChars)
-{//remove a numberOfChars...
+{ // remove a numberOfChars...
   for(int i=0;i<numberOfChars;i++){keyOut(BACKSPACE);}
 }
 
 //----------------adjusting settings with pontentiometer---------
-//set ADJUST_POT in pin_definitions.h
-#define PWM_ADJUST 4
-#define TIMING_ADJUST 5
-void potentiometer(byte mode)
+// set ADJUST_POT in pin_definitions.h
+
+void potentiometer(byte mode)             // Main loop potentiometer check
 {
-  static byte adjustMode = PWM_ADJUST;
-  int potValue = analogRead(ADJUST_POT);
-  
-  if(mode == MONITOR_MODE){mode=adjustMode;}//check to do adjustments 
-  if(mode == DEFAULT_MODE){potReturn(potValue);}
-  else if (mode == ADJUST_PWM)
-  {
-    adjustMode = PWM_ADJUST;
-    potReturn(potValue);
+  static boolean intensity = true;        // default adjust mode: intensity
+  int potValue = analogRead(ADJUST_POT);  // read current pot value
+
+  if(mode == MONITOR_MODE)
+  { // check to do adjustments on either intensity or spacing
+    if (intensity) {pagerIntensity(map(potValue, 0, 1023, 0, 4095));}
+    else {hapticMessage(0, map(potValue, 0, 1023, 10, 500));}
   }
-  else if (mode == ADJUST_TIMING)
+  else if (mode == DEFAULT_MODE){potReturn(potValue);} // "flash value"
+  else if (mode == ADJUST_PWM)        // intensity mode case
   {
-    adjustMode = TIMING_ADJUST;
-    potReturn(potValue);
+    intensity = true;                 // set function to adjust intensity
+    potReturn(potValue);              // "flash" current value
   }
-  else if (mode == PWM_ADJUST)
-  {pagerIntensity(map(potValue, 0, 1023, 0, 4095));}
-  else if (mode == TIMING_ADJUST)
-  {hapticMessage(0, map(potValue, 0, 1023, 10, 500));}
+  else if (mode == ADJUST_TIMING)     // timing/spacing mode case
+  {
+    intensity = false;                // set function to adjust timing
+    potReturn(potValue);              // "flash" current value
+  }
 }
 
 void potReturn(int potValue)
 {
   byte rawNumber = map(potValue,0,1023,0,9);
-  keyOut(rawNumber + 48); // turn the raw number into an ascii number
-  hapticMessage(rawNumber + 48); // start feedback
-  while(!hapticMessage(MONITOR_MODE)){;} //break loop when letter is finished
-  keyOut(BACKSPACE); //key message
+  keyOut(rawNumber + 48);                // convert to ascii number
+  hapticMessage(rawNumber + 48);         // start feedback
+  // TODO !!!!! BLOCKING OPPERATION !!!!!!! <-------
+  while(!hapticMessage(MONITOR_MODE)){;} // break loop when letter is finished
+  keyOut(BACKSPACE);                     // key message
 }
